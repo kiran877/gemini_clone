6910f89f93fa5102fcd90e8a58460a02504e75ef
kiran877 <103376796+kiran877@users.noreply.github.com>
2024-07-19 12:56:32 +0530

    commit2

--- src/Config/Gemini.js
+++ src/Config/Gemini.js
@@ -2,8 +2,9 @@
 const {
     GoogleGenerativeAI,
   } = require("@google/generative-ai");
-  
-  const apiKey = process.env.GEMINI_API_KEY;
+
+   const apiKey="AIzaSyBh4vLYmuQONgIrhgT6J0eirhQGkV_p2YU";
+  //const apiKey = process.env.GEMINI_API_KEY;
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
